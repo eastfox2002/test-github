1a04b931bb9d4e8b6ca266f48ccbac3dc014324c
eastfox2002 <eastfox2002@163.com>
2013-05-09 14:34:12 +0800

    second

---
+++ wordplay copy 2.rb
@@ -0,0 +1,8 @@
+ class WordPlay
+ end
+ class String
+ 	def sentences
+ 		gsub(/\n|\r/, ' ').split(/\.\s*/)
+ 	end
+ end
+ %q{Hello. This is a test of basic sentence splitting. It even works over multiple lines.}.sentences
